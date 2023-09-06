db4bb191ef4f4b343abe99f21328e7598e39ddaf
room <evan.wever@gmail.com>
2023-09-05 17:09:55 -0700

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci {
+
+    public static int calculateFibonacci(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int nthTerm = calculateFibonacci(n);
+        System.out.println("The " + n + "th term of the Fibonacci sequence is " + nthTerm + ".");
+    }
+}
